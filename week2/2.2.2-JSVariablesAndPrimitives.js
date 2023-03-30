8a635bdfd73e4a696d7d96a0f8c9a1c5fa56b004
kwilkes314 <kendallwilkes.kw@gmail.com>
2023-03-30 15:40:36 -0500

    Js variables 2.2.2 homework

---
+++ week2/2.2.2-JSVariablesAndPrimitives.js
@@ -0,0 +1,34 @@
+// create a variable called "name" that references a string
+let name="kyrie";
+// create a variable called "favoriteSong" that references a string
+let favoriteSong="how it feel";
+// create a variable called "wage" that references a number
+let wage= 112000;
+// create a variable called "age" that references a number
+let age=28;
+// create a variable called "onlyChild" that references a boolean
+let onlyChild= true;
+// create a variable called "satisfied" that references a boolean
+let satisfied= true;
+// create a variable called "favoriteThing" that references ANY primitive value
+// print the data type of "favoriteThing"
+let favoriteThing="relentless";
+console.log(favoriteThing);
+// You heard a new song that became your favorite.
+// Reassign the variable "favoriteSong"
+favoriteSong="no love";
+// You no longer like the type of data stored in "favoriteThing"
+// Reassign "favoriteThing" to a different primitive data type and print its type
+favoriteThing=3;
+console.log(favoriteThing);
+// Happy Birthday!
+// Reassign "age" to a new value
+age=29;
+// You got a big raise at work.
+// Reassign "wage" to a new value
+wage=1350000;
+// Your raise changed your job satisfaction.
+// Assign "satisfied" to true
+satisfied=true;
+// use "console.log()" and "+" to print a sentence that includes two of the above variables
+console.log("Hi i am "  + name + " and I am " + age + " years old.");
