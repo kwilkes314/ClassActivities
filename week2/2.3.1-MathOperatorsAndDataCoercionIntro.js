0fa98b4d73076097df3a0be9aa3a66758423a7ae
kwilkes314 <kendallwilkes.kw@gmail.com>
2023-04-03 19:27:51 -0500

    Week3 3.1.1 activity

---
+++ week2/2.3.1-MathOperatorsAndDataCoercionIntro.js
@@ -0,0 +1,48 @@
+// Logical Operators
+// what do you expect each line to print?
+console.log(true && false);
+
+console.log(true || false);
+
+console.l
+console.log(!(true && false));
+//true
+console.log(false || !false);
+//true
+
+// Logical Operators and Truthy/Falsy
+// ? what do you expect each line to print?
+console.log(undefined || null);
+console.log(!``);
+console.log(!(1 && "false"));
+console.log(NaN || !"true");
+
+// Set the age variable below equal to a number,
+// then use conditional operators to construct an expression that returns...
+// "full menu" if the age is over 10,
+// and "kids menu" if the age is less than 10
+
+let age;
+age=32;
+if(age>10){
+  console.log("full menu")
+} else if(age<10){
+  console.log("kids menu")
+}
+
+switch(age){
+  case age>10 :{
+    console.log("full menu")
+    break;
+  }
+  case age
+}
+
+//age>10? console.log("full menu") : console.log("kids menu");
+// Using only logical operators (no comparison operators),
+// write an if statement that prints...
+//  "The number is zero" when num is 0.
+
+let num = 0;
+
+// Write your if statement here *
